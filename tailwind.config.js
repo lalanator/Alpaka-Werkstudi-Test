e5e2de00f6d479b6c120f121d9477e7b31a2d8c2
lalanator <lea.strauch@gmx.de>
2023-10-25 14:00:02 +0200

    moved images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [
-    require('./presets.js')
-  ],
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 }
